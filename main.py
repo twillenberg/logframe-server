00c64fa9181ba76863ca38d2b60eb818b70a200f
Tony Willenberg <tony.willenberg@gmail.com>
2016-10-16 14:19:58 +1100

    Added support for Jinja2.

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import webapp2
-import 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, from the Google App Engine!')
+from jinja2 import Template
+
+env = Environment(loader=PackageLoader('webtester', 'templates'))
 
 class Welcome(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('''
-            <!DOCTYPE html>
-            <html>
-            <head>
-            <link href="/default.css" rel="stylesheet" type="text/css">
-            <title>LFS</title>
-            </head>
-            <body>
-            <p class='section-heading'>Welcome to the Logical Framework Server</p>
-            <p class='normal'>This is a prototype demonstrating: 
-            <br>1. a service running on the Google App Engine application hosting platform.
-            <br>2. routing from a subdomain name to the applicaiton.
-            </p>
-            </body>
-            </html>
-        ''')        
+        template = env.get_template('default.html')
+
+        template = Template("""""")
+        self.response.write(template.render(app_name="Logical Framework Server"))        
 
 class Login(webapp2.RequestHandler):
     def get(self):
@@ -42,7 +28,7 @@ class Login(webapp2.RequestHandler):
             </html>
         ''')    
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', Welcome),
     ('/welcome', Welcome),
     ('/login',Login)
 ], debug=True)
