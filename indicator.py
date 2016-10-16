a24c57aaf830f403d67e95f4ba95e4dfaddbee55
Tony Willenberg <tony.willenberg@gmail.com>
2016-10-16 22:14:22 +1100

    Installed libraries.

--- indicator.py
+++ indicator.py
@@ -1,7 +1,11 @@
 import debug
-#import requests
 import json
+import requests
+import requests_toolbelt.adapters.appengine
 
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
 
 
 class Indicator(object):
